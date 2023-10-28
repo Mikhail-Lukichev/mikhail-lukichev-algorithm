9a01f5a37ba747a06508cd251b4d0e205f0d172d
Mikhail Lukichev <dextrumm@gmail.com>
2023-10-28 14:22:37 -0600

    Initial commit

---
+++ src/main/java/pro/sky/mikhaillukichevalgorithm1/MikhailLukichevAlgorithm1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.mikhaillukichevalgorithm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikhailLukichevAlgorithm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikhailLukichevAlgorithm1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/mikhaillukichevalgorithm1/MikhailLukichevAlgorithm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.mikhaillukichevalgorithm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhailLukichevAlgorithm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
